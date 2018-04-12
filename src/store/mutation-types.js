8ba0f1241c9e17b31a72bbf7bfd41c066bc8413e
KanKK <286454796@qq.com>
2018-04-12 23:57:06 +0800

    refactor: 添加mutation-types

---
+++ src/store/mutation-types.js
@@ -0,0 +1,8 @@
+// user
+export const USER_LOGIN = 'USER_LOGIN'
+export const USER_LOGOUT = 'USER_LOGOUT'
+
+// pictures
+export const PICTURES_INIT = 'PICTURES_INIT'
+export const PICTURES_ADD = 'PICTURES_ADD'
+export const PICTURES_DELETE = 'PICTURES_DELETE'
